9ee9daacdd6a22e90b4618f2d24768cffd6ef711
Vinicius <vinnicius.jimenez@gmail.com>
2020-11-24 10:57:41 -0300

    Using middlewares

---
+++ using-middleware/index.js
@@ -0,0 +1,38 @@
+const { request, response } = require('express')
+const express = require('express')
+
+const app = express()
+const router = express.Router()
+
+router.use((request, response, next) => {
+    console.log(`Time: ${Date.now()}`)
+    next()
+})
+
+router.use('/user/:id', (request, response, next) => {
+    console.log(`Request URL: ${request.originalUrl}`)
+    next()
+},
+(request, response, next) => {
+    console.log(`Request Type: ${request.method}`)
+    next()
+})
+
+router.get('/user/:id', (request, response, next) => {
+   request.params.id === '0' ? next('route') : next()
+},
+(request, response, next) => {
+    response.send('<h1>Regular Page</h1>')
+})
+
+// Caso o ID seja igual a 0
+
+router.get('/user/:id', (request, response) => {
+    console.log(request.params.id)
+
+    response.send('<h1>Special Page</h1>')
+})
+
+app.use('/', router)
+
+app.listen(3000)
